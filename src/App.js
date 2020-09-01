8296d2451be233fcda6a5253e56f856d9abb2df6
DeepshikaS <ddeepu13@gmail.com>
2020-09-01 23:00:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+toast.configure();
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      board: ["", "", "", "", "", "", "", "", ""],
+      player_turn: "X",
+    };
+  }
+
+  squareClicked(index) {
+    let player_turn = this.state.player_turn;
+    let board = this.state.board;
+    board[index] = player_turn;
+    let winning_combinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winning_combinations.length; i++) {
+      let winning_row = winning_combinations[i];
+      let p1 = winning_row[0];
+      let p2 = winning_row[1];
+      let p3 = winning_row[2];
+      if (
+        board[p1] != "" &&
+        board[p1] == board[p2] &&
+        board[p2] == board[p3] &&
+        board[p3] == board[p1]
+      )
+        toast.success(`winner ${player_turn} `, {
+          position: toast.POSITION.TOP_CENTER,
+        });
+    }
+
+    player_turn = player_turn == "X" ? "O" : "X";
+
+    this.setState({
+      player_turn: player_turn,
+      board: board,
+    });
+
+    console.log(index);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Tic Tac Toe</h1>
+        <div className="board">
+          {this.state.board.map((square, index) => {
+            return (
+              <div onClick={() => this.squareClicked(index)} className="square">
+                <h3 className="symbol">{square}</h3>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;

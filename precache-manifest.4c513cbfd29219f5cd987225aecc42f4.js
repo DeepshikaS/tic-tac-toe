45fe45b5413a4f8b6b4c81805d024eed9f1b9802
DeepshikaS <ddeepu13@gmail.com>
2020-09-04 22:21:12 +0530

    Updates

---
+++ precache-manifest.4c513cbfd29219f5cd987225aecc42f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59b303096528fb3e2a8c9c90004d77a",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "008402a4a5fea1a56180",
+    "url": "/tic-tac-toe/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "5e2c06ae9139712b9d10",
+    "url": "/tic-tac-toe/static/css/main.a5d3fbdc.chunk.css"
+  },
+  {
+    "revision": "008402a4a5fea1a56180",
+    "url": "/tic-tac-toe/static/js/2.b255fac5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tic-tac-toe/static/js/2.b255fac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2c06ae9139712b9d10",
+    "url": "/tic-tac-toe/static/js/main.52b51031.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
